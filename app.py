4531ed7093710d195191e8806423e85d7fc3bd18
Benoit <benoit.arliaud@nodya-group.fr>
2021-08-28 17:58:30 +0200

    add shape selection mode

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import tkinter
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from matplotlib import pyplot as plt
-from geometry.geometry import billard
+from geometry.geometry import billard, SHAPE
 import numpy as np
 
 class App:
@@ -21,23 +21,38 @@ class App:
                                         command=self.increase)
         self.button_right.pack(side="left")
 
+        self.button_x = tkinter.Button(self.frame,text="X : On/Off",
+                                        command=self.x_switch)
+        self.button_x.pack(side="left")
+        self.x_state = True
+        self.button_y = tkinter.Button(self.frame,text="Y : On/Off",
+                                        command=self.y_switch)
+        self.button_y.pack(side="right")
+        self.y_state = True
+
+
+        self.button_shape = {}
+        self.shape_choice = tkinter.StringVar(self.frame)
+        self.shape_choice.set("carre")
+        self.shape_list = tkinter.OptionMenu(self.frame, self.shape_choice, *SHAPE.keys(), command=self.change_shape)
+        self.shape_list.pack(side="top")
+
+
         fig, (self.ax,self.ax2) = plt.subplots(ncols=2, figsize=(10,5))
 
         # Create billard
-        self.b=billard([[0,0], [0,1], [1,1]], position=[0,0], slope=[1,1.1])
+        self.b=billard([[0,0], [0,1], [1,1]], position=[0.5,0.5], slope=[1,1.1])
         self.b.n_bounce()
         xs, ys = zip(*(self.b.corners+[self.b.corners[0]]))
         self.box, = self.ax.plot(xs, ys, c="k")
         x, y = zip(*self.b.bounces)
         self.line, = self.ax.plot(x,y, c="g")
-        self.x_line, = self.ax2.plot(self.b.interval,x, c='b')
+        self.x_line, = self.ax2.plot(self.b.interval,x, c='k')
         self.y_line, = self.ax2.plot(self.b.interval,y, c='g')
-
 
         self.canvas = FigureCanvasTkAgg(fig,master=master)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(side='top', expand=1)
-        
         self.frame.pack()
 
         self.i = 0
@@ -45,44 +60,57 @@ class App:
     def update_plot(self):
         x, y = zip(*self.b.bounces)
         self.line.set_data(x, y)
-
         self.x_line.set_data(self.b.interval,x)
         self.y_line.set_data(self.b.interval,y)
-
         self.canvas.draw()
 
     def updateValue(self, event):
         sg = self.slider.get()/1000
         t = 1+round(np.tan(sg)*1000)/1000
-
         self.b.reset_slope([1, t])
         self.b.n_bounce()
         self.update_plot()
         
     def decrease(self):
         self.i -= 0.001
-        self.change_shape()
+        self.transform_shape()
 
     def increase(self):
         self.i += 0.001
-        self.change_shape()
+        self.transform_shape()
 
+    def change_shape(self, event):
+        shape = SHAPE[self.shape_choice.get()]
+        self.b=billard(shape, position=self.b.bounces[0], slope=[1,1])
+        self.b.n_bounce()
+        self.reset_view()
+        self.update_plot()
 
-    def change_shape(self):
+    def transform_shape(self):
         self.b=billard([[0,0], [self.i,1], [1+self.i,1], [1,0]], position=[0,0], slope=[1,1])
         sg = self.slider.get()/1000
         t = 1+round(np.tan(sg)*1000)/1000
         self.b.reset_slope([1,t])
         self.b.n_bounce()
         
+        self.reset_view()
+        self.update_plot()
+
+    def reset_view(self):
         x, y = zip(*(self.b.corners+[self.b.corners[0]]))
         self.box.set_data(x, y)
-        a = max(self.i+1,1)-min(self.i, 0)
         self.box.axes.set_xlim(min(self.i, 0),max(self.i+1,1))
         self.line.axes.set_xlim(min(self.i, 0),max(self.i+1,1))
-        
+
+    def x_switch(self):
+        self.x_state = not self.x_state
+        self.x_line.set_visible(self.x_state)
         self.update_plot()
-        self.canvas.draw()
+        
+    def y_switch(self):
+        self.y_state = not self.y_state
+        self.y_line.set_visible(self.y_state)
+        self.update_plot()
 
 
 root = tkinter.Tk()
